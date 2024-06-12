b36acb53f839fdead04da0134c81ceb5aaf0e086
Cameron <20095325@tafe.wa.edu.au>
2024-06-12 21:25:04 +0800

    upload readme and example config

---
+++ README.md
@@ -0,0 +1,1 @@
+# Arduino Class Question Queue system
---
+++ example.Config.h
@@ -0,0 +1,8 @@
+#define WIFI_SSID     "your_wifi_name"
+#define WIFI_PASS     "your_wifi_password"
+#define AIO_SERVER    "io.adafruit.com"
+#define AIO_SERVERPORT 1883
+#define AIO_USERNAME  "your_adafruit_username"
+#define AIO_KEY       "your_adafruit_key"
+#define AIO_FEED      "your_adafruit_update_feed"
+#define AIO_FEED_SUBSCRIBE      "your_adafruit_subscribe_feed"
